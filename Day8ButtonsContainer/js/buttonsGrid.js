50f209052cd2f3c3d49f4318f8a245fc0839f2b1
dongmin7208 <sayg3o3d@gmail.com>
2022-06-23 23:57:57 +0900

    Buttons Container

---
+++ Day8ButtonsContainer/js/buttonsGrid.js
@@ -0,0 +1,18 @@
+document.getElementById("btn5").addEventListener("click", function () {
+    let temp = document.getElementById("btn1").innerHTML;
+    document.getElementById("btn1").innerHTML =
+        document.getElementById("btn4").innerHTML;
+    document.getElementById("btn4").innerHTML =
+        document.getElementById("btn7").innerHTML;
+    document.getElementById("btn7").innerHTML =
+        document.getElementById("btn8").innerHTML;
+    document.getElementById("btn8").innerHTML =
+        document.getElementById("btn9").innerHTML;
+    document.getElementById("btn9").innerHTML =
+        document.getElementById("btn6").innerHTML;
+    document.getElementById("btn6").innerHTML =
+        document.getElementById("btn3").innerHTML;
+    document.getElementById("btn3").innerHTML =
+        document.getElementById("btn2").innerHTML;
+    document.getElementById("btn2").innerHTML = temp;
+});
